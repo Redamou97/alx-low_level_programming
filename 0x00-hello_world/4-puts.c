655a5c73946ef912b0e53c7cce1fa7d5ea6f70ba
Reda Moutazaki <redamoutazaki@gmail.com>
2023-06-15 18:03:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A program that print with puts and function
+ * Return: 0 (Success)
+ */
+init main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
