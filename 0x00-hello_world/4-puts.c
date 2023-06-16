a34be3cf3c889eb62ceb060d08c098a97505f7cb
Reda Moutazaki <redamoutazaki@gmail.com>
2023-06-15 18:06:13 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main -A program that print with puts and function
  * Return: 0 (Success)
  */
-init main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
