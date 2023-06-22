4c9034bd2045330c243cd8859266805374e56081
Reda Moutazaki <redamoutazaki@gmail.com>
2023-06-22 16:58:19 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integres
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return rusltat
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
