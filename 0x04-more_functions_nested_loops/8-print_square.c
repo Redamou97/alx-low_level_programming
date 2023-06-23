d1a532b7b83842565441be37e3c0f50760347f2e
Reda Moutazaki <redamoutazaki@gmail.com>
2023-06-22 18:54:39 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using the chracter #
+ *
+ * @size: is the size of the square
+ *
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	int row, colum;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (colum = 1; colum <= size; colum++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
