18d6d3308126650c0ed1c2a122f46449ad040c16
Reda Moutazaki <redamoutazaki@gmail.com>
2023-06-23 08:41:10 -0700

    times square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,16 +5,16 @@
  *
  * @size: is the size of the square
  *
- * Return: Always 0
+ * Return: empty
  */
 
 void print_square(int size)
 {
 	int row, colum;
 
-	for (row = 1; row <= size; row++)
+	for (row = 0; row < size; row++)
 	{
-		for (colum = 1; colum <= size; colum++)
+		for (colum = 0; colum < size; colum++)
 			_putchar('#');
 		_putchar('\n');
 	}
