8bb38a4e425db0b5cd4864f367338edcfbd320a2
Reda Moutazaki <redamoutazaki@gmail.com>
2023-06-23 16:35:21 -0700

    prime 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,64 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
 /**
- * _sqrt - finds the square root
- *
- * 0x: input number
- *
- * Return: square root of x
- */
-
-double _sqrt(double x)
-{
-	float sqrt, tmp;
-	sqrt = x / 2;
-	tmp = 0;
-
-	while (sqrt != tmp)
-	{
-		tmp = sqrt;
-		sqrt = (x / tmp + tmp) / 2;
-	}
-	return (0);
-}
-
-/**
- * largest_prime_factor - finds and prints the largest
- *                      prime factor of number (num)
- *
- * @num: number to find
- */
-
-void largest_prime_factor(long int num)
-{
-	int prmNu, largest;
-
-	/* frist divide with the smallest prime number (two) */
-	while (num % 2 == 0)
-		num = num / 2;
-	/* num must be add so we proceed to the next prime number (plus two) */
-	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
-	{
-		while (num % prmNu == 0)
-		{
-			num = num / prmNu;
-			largest = prmNu;
-		}
-	}
-
-	if (num > 2)
-		largest = num;
-	printf("%d\n", largest);
-}
-
-/**
- * main - Entry point
- *
+ * main - Prints the largest prime factor of the number 612852475143
  * Return: Always 0
  */
+
 int main(void)
 {
-	largest_prime_factor(612852475143);
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
 
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
 	return (0);
 }
