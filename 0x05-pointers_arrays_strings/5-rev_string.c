3f3dcbc92eea44ba41a3cd0e4367293ac1c18bac
Reda Moutazaki <redamoutazaki@gmail.com>
2023-06-26 11:32:01 -0700

    pointers_arrays

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: parmeter reverses string
+ * rusltat: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (l = 0; s[1] != '\0'; ++i)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
