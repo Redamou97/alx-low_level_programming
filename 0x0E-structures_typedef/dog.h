027560312e4223312b0a89dbe76b53983ea025d3
Reda Moutazaki <redamoutazaki@gmail.com>
2023-07-17 17:27:20 -0700

    dog2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,11 +17,11 @@ struct dog
 /**
  * _dog - Typedef for dog structure
  */
-typedef struct dog _dog;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-_dog *new_dog(char *name, float age, char *owner);
-void free_dog(_dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
